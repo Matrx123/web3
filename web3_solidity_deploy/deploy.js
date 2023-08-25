a4078eec00fd3811ea73e4758ab697960154d66e
vipin joshi <vpnjsh55@gmail.com>
2023-08-25 11:41:03 +0530

    deploy contract using ethers and ganache

--- web3_solidity_deploy/deploy.js
+++ web3_solidity_deploy/deploy.js
@@ -1,1 +1,36 @@
+const { ethers } = require("ethers");
+const fs = require("fs");
+
+async function main() {
+  //HTTP://127.0.0.1:7545
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+  //creating a wallet with the private key from ganache
+  const wallet = new ethers.Wallet(
+    "private key of your ganache ethereum blockchain",
+    provider
+  );
+  //fetch abi
+  const abi = fs.readFileSync(
+    "./SimpleContract_sol_SimpleContract.abi",
+    "utf8"
+  );
+  //fetch binary
+  const binary = fs.readFileSync(
+    "./SimpleContract_sol_SimpleContract.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait....");
+  const contract = await contractFactory.deploy();
+  //use the methods in the contracts
+  const favNo = await contract.retrieve();
+  console.log("favourite no >>>", favNo.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log("Error >>>", error);
+    process.exit(1);
+  });
 
