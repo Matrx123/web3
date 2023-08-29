3572606ffeb3ca8344ef558f9bb9bc3d02b6a8a6
vipin joshi <vpnjsh55@gmail.com>
2023-08-29 12:10:17 +0530

    added code to verify and deploy on testnet sepolia

--- hardhat-simple-storage/scripts/deploy.js
+++ hardhat-simple-storage/scripts/deploy.js
@@ -1,33 +1,46 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+//imports
+const {ethers, run, network} = require("hardhat");
 
+
+//async main
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const simpleContractFactory = await ethers.getContractFactory("SimpleContract");     
+  console.log("Deploying Contract ....");
+  const simpleStorage = await simpleContractFactory.deploy();
+  const _address = await simpleStorage.getAddress();
+  console.log(`Deployed contract to: ${_address}`);
+  console.log(network.config);
+  //should only be in testnet or mainnet
+  await simpleStorage.deploymentTransaction().wait(6);
+  await verify(_address, []);
+  //interact with contract
+  const currentValue = await simpleStorage.retrieve();
+  console.log('Current value: ', currentValue.toString());
+  //update the current value
+  const txnResponse = await simpleStorage.store(7);
+  await txnResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log('Updated Value: ', updatedValue.toString());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+  console.log("Verifying contract ....");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      contructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes('already verified')){
+      console.log('Already verified');
+    }else{
+      console.log(error);
+    }
+  }
+
+}
+
+//main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => process.exit(1));
