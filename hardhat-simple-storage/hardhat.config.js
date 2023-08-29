acd6f25a90414dfa0466ee8f81c49a3db95a7a17
vipin joshi <vpnjsh55@gmail.com>
2023-08-29 12:07:58 +0530

    added testnet networks and etherscan api key for verfying contracts

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RPC_URL = process.env.RPC_URL;
+const PVT_KEY = process.env.PVT_KEY;
+const ETH_SCN_KEY = process.env.ETH_SCN_API_KEY
+
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks:{
+    sepolia :{
+      url : RPC_URL,
+      accounts: [PVT_KEY],
+    }
+  },
+  etherscan:{
+    apiKey : ETH_SCN_KEY
+  },
+  solidity: "0.8.8",
 };
