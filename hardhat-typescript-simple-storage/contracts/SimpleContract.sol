b116b6a9e6173c3fbbe194c78c478ed1441342e6
vipin joshi <vpnjsh55@gmail.com>
2023-09-08 11:08:21 +0530

    Add files via upload

---
+++ hardhat-typescript-simple-storage/contracts/SimpleContract.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleContract {
+    uint256 favouriteNo;
+    mapping(string => uint256) public nameToFavNo;
+    People[] public peoples;
+
+    struct People {
+        string name;
+        uint256 no;
+    }
+
+    function store(uint256 _no) public virtual {
+        favouriteNo = _no;
+    }
+
+    function addPeople(string memory _name, uint256 _no) public {
+        peoples.push(People(_name, _no));
+    }
+
+    function makeMapping(string memory _name, uint256 _no) public {
+        nameToFavNo[_name] = _no;
+    }
+
+    function getPerson(uint256 _index) public view returns (People memory) {
+        return peoples[_index];
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favouriteNo;
+    }
+}
---
+++ hardhat-typescript-simple-storage/hardhat.config.ts
@@ -0,0 +1,37 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify")
+require("dotenv").config()
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RPC_URL = process.env.RPC_URL
+const PVT_KEY = process.env.PVT_KEY
+const ETH_SCN_KEY = process.env.ETH_SCN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: RPC_URL,
+            accounts: [PVT_KEY],
+        },
+        dev: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETH_SCN_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    solidity: "0.8.8",
+}
