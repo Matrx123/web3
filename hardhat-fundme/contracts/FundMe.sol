b4aafdef66d92627be771aeefbf69460602e3d3f
vipin joshi <vpnjsh55@gmail.com>
2023-09-09 12:13:46 +0530

    Update FundMe.sol

--- hardhat-fundme/contracts/FundMe.sol
+++ hardhat-fundme/contracts/FundMe.sol
@@ -11,10 +11,18 @@ contract FundMe {
 
     address[] public funders;
     mapping(address => uint256) public addressToAmount;
+    //aggregator address variable
+    AggregatorV3Interface public priceFeed;
+    address public owner;
+
+    constructor(address priceFeedAddress) {
+        owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= minimumUsd,
+            msg.value.getConversionRate(priceFeed) >= minimumUsd,
             "Didn't send enough!"
         );
         funders.push(msg.sender);
@@ -22,21 +30,21 @@ contract FundMe {
     }
 
     function withdraw() public {
-        for (uint256 i = 0; i < funders.length; i++) {
-            addressToAmount[funders[i]] = 0;
-        }
-        //reset the array
-        funders = new address[](0);
-        //withdraw
-        //transfer
-        payable(msg.sender).transfer(address(this).balance);
-        //send
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess, "Send Failed");
-        //call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "Call Failed");
+        // for (uint256 i = 0; i < funders.length; i++) {
+        //     addressToAmount[funders[i]] = 0;
+        // }
+        // //reset the array
+        // funders = new address[](0);
+        // //withdraw
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // //send
+        // (bool sendSuccess, ) = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+        // //call
+        // (bool callSuccess, ) = payable(msg.sender).call{
+        //     value: address(this).balance
+        // };
+        // require(callSuccess, "Call Failed");
     }
 }
