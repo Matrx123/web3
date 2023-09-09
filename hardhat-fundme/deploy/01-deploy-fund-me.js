15f2137ca2a426b97e44fc4d0c8b8420466b3070
vipin joshi <vpnjsh55@gmail.com>
2023-09-09 12:12:24 +0530

    Update 01-deploy-fund-me.js

--- hardhat-fundme/deploy/01-deploy-fund-me.js
+++ hardhat-fundme/deploy/01-deploy-fund-me.js
@@ -1,4 +1,18 @@
-function deployFunc() {
-    console.log("hi");
+const { network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+
+    //make mocks when develop locally
+    const fundme = await deploy("FundMe", {
+        contract: "FundMe",
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        logs: true,
+    })
 }
-module.exports.default = deployFunc
