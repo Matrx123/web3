526dda14b24634123aee64d6acc1f5d05f0d71a3
vipin joshi <vpnjsh55@gmail.com>
2023-09-11 11:56:27 +0530

    Updated withdraw function

--- hardhat-fundme/contracts/FundMe.sol
+++ hardhat-fundme/contracts/FundMe.sol
@@ -23,6 +23,9 @@ contract FundMe {
     AggregatorV3Interface public priceFeed;
     address public owner;
 
+    /**
+     * Only owner modifier to check if owner else throw error
+     */
     modifier onlyOwner() {
         if (msg.sender != owner) revert FundMe__NotOwner();
         _;
@@ -33,31 +36,35 @@ contract FundMe {
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    /**
+     * @notice this function send funds
+     * @dev this is create a list of funders and their contributions
+     */
     function fund() public payable {
         require(
             msg.value.getConversionRate(priceFeed) >= minimumUsd,
-            "Didn't send enough!"
+            "You need to spend more ETH!"
         );
         funders.push(msg.sender);
         addressToAmount[msg.sender] = msg.value;
     }
 
     function withdraw() public {
-        // for (uint256 i = 0; i < funders.length; i++) {
-        //     addressToAmount[funders[i]] = 0;
-        // }
-        // //reset the array
-        // funders = new address[](0);
-        // //withdraw
-        // //transfer
+        for (uint256 i = 0; i < funders.length; i++) {
+            addressToAmount[funders[i]] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
+        //withdraw
+        //transfer
         // payable(msg.sender).transfer(address(this).balance);
-        // //send
-        // (bool sendSuccess, ) = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send Failed");
-        // //call
-        // (bool callSuccess, ) = payable(msg.sender).call{
-        //     value: address(this).balance
-        // };
-        // require(callSuccess, "Call Failed");
+        //send
+        //(bool sendSuccess, ) = payable(msg.sender).send(address(this).balance);
+        //require(sendSuccess, "Send Failed");
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call Failed");
     }
 }
