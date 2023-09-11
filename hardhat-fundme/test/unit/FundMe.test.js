f6b3071b41df2e85686f0b11bd90db804af43fe5
vipin joshi <vpnjsh55@gmail.com>
2023-09-11 13:08:12 +0530

    added test for other functions

--- hardhat-fundme/test/unit/FundMe.test.js
+++ hardhat-fundme/test/unit/FundMe.test.js
@@ -1,11 +1,12 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
 
-describe("FundMe", () => {
+describe("FundMe", function () {
     let fundMe
     let deployer
     let mockV3Aggregator
-    beforeEach(async () => {
+    const sendValue = ethers.parseEther("1") // 1 ETH
+    beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
         fundMe = await ethers.getContract("FundMe", deployer)
@@ -15,9 +16,55 @@ describe("FundMe", () => {
         )
     })
     describe("constructor", () => {
-        it("sets the aggregator address correctly", async () => {
+        it("sets the aggregator address correctly", async function () {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.target)
+        })
+    })
+
+    describe("fund", function () {
+        it("Fails when we dont send enough ETH", async function () {
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH!",
+            )
+        })
+        it("updated the amount funded data structure", async function () {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressToAmount(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Adds funder to array of funders", async function () {
+            await fundMe.fund({ value: sendValue })
+            const funder = await fundMe.funders(0)
+            assert.equal(funder, deployer)
+        })
+        describe("withdraw", function () {
+            this.beforeEach(async function () {
+                await fundMe.fund({ value: sendValue })
+            })
+            it("Withdraw from a single account type list", async function () {
+                //initial balances
+                const initialFundMeBalance = await ethers.provider.getBalance(
+                    fundMe.target,
+                )
+                const initialDeployerBalance =
+                    await ethers.provider.getBalance(deployer)
+                //withdraw balance from single account list
+                const txn = await fundMe.withdraw()
+                await txn.wait(1)
+                //final balances
+                const finalFundMeBalance = await ethers.provider.getBalance(
+                    fundMe.target,
+                )
+                const finalDeployerBalance =
+                    await ethers.provider.getBalance(deployer)
+                const gasCost = txn
+                assert.equal(finalFundMeBalance, 0)
+                assert.equal(
+                    initialFundMeBalance.add(initialDeployerBalance).toString(),
+                    finalDeployerBalance.add(txn.gasPrice).toString(),
+                )
+            })
         })
     })
 })
